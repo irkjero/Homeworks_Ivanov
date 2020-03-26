65182950de5cd701c8cbdd441d9a4435aa0a3004
Peter <pivanovs@mail.ru>
2020-03-26 10:25:33 +0700

    create project

---
+++ parser_csv/cmake-build-debug/CMakeFiles/parser_csv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser_csv.dir/main.cpp.o"
+  "parser_csv"
+  "parser_csv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parser_csv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
