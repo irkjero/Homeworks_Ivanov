88ba3dbe087c0216d77cff95d69fc26e6d0ff67f
Peter <pivanovs@mail.ru>
2020-04-09 11:43:31 +0700

    init commit

---
+++ Integtal/cmake-build-debug/CMakeFiles/Integtal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Integtal.dir/main.cpp.o"
+  "Integtal"
+  "Integtal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Integtal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
