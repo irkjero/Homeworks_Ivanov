2cb6a635405f663c54b4c6472493f465f9390818
Peter <pivanovs@mail.ru>
2020-04-14 18:51:38 +0700

    New method

--- Integtal/Gausses.h
+++ Integtal/Gausses.h
@@ -20,4 +20,9 @@ struct Order55{
     constexpr static int order = 5;
 };
 
+struct gauss{
+    static constexpr auto points = std::make_tuple(0.0, 0.538469, -0.538469, 0.9061798, -0.9061798);
+    static constexpr auto weights =  std::make_tuple(0.568889,  0.478629, 0.478629, 0.236927, 0.236927);
+};
+
 #endif //INTEGTAL_GAUSSES_H
--- Integtal/main.cpp
+++ Integtal/main.cpp
@@ -20,10 +20,53 @@
 //}
 
 
+template<typename T>
+struct get_size
+{
+    template<typename Func, typename T1, typename T2>
+    constexpr static auto get_elems(Func func, T1& w,  T2& p){
+        return func(std::get<0>(p))*std::get<0>(w) ;
+    }
+};
+
+template<typename T, typename ...Ts>
+struct get_size<std::tuple<T, Ts...>>
+{
+    template<typename Func, typename T1, typename T2>
+    constexpr static auto get_elems(Func func, T1& w, T2& p){
+        constexpr int size = sizeof...(Ts);
+        return func(std::get<size>(p))*std::get<size>(w) + get_size< std::tuple<Ts...> >::get_elems(func, w, p);//get_elems<Func,T1, T2>(func, w, p);
+    }
+};
+//
+template<typename T>
+constexpr auto sizee(T tuple){
+    return std::tuple_size<T>::value;
+}
+
+template<typename Func, typename T>
+constexpr auto call(Func func, T weights, T points){
+    return get_size<T>::get_elems(func, weights, points);
+}
+
+template<typename Function, typename ord>
+constexpr auto integrate_2(Function func, ord int_w_p){
+    auto res = call<Function>(func, int_w_p.weights, int_w_p.points);
+    return res;
+}
+
+
 int main() {
 //    constexpr auto f = my_function<double>;
 //    std::cout << Integrate(f) <<std::endl;
 //    static_assert(Integrate(f)-83.333 < 1e-3 and Integrate(f)-83.333 >= 0, "good");
+
+    auto f = [](std::tuple<double> arg){ return std::get<0>(arg);};
+    auto f_2 = [](double x){ return x*x;};
+
+    constexpr auto res = integrate_2(f_2, gauss{});
+    std::cout << "Newbee: "<< res << std::endl;
+    static_assert(res - 0.6666 < 1e-4 and res - 0.6666 >= 0);
 
     std::cout << Integrate<square, Order33>() << std::endl;
     static_assert(Integrate<square, Order33>() - 0.6666 < 1e-4 and Integrate<square, Order33>() - 0.6666 >= 0);
